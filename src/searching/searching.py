bb0f191eecbff3620ed8beb10481dba322078457
Richard Machado <mmachado.richard@gmail.com>
2020-08-17 13:26:28 -0700

    done

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,27 @@
 def linear_search(arr, target):
-    # Your code here
-
+    for item in arr:
+        if item == target:
+            return arr.index(item)
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    start = 0
+    end = (len(arr) - 1)
 
-    # Your code here
+    while start <= end:
+        middle = (start + end) // 2
+        check = arr[middle]
 
+        if check == target:
+            return arr.index(check)
+        if check > target:
+            end = middle - 1
+        if check < target:
+            start = middle + 1
 
     return -1  # not found
+
+
